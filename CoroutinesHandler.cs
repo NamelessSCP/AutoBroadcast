3cc6b267fa6637b5ee10c48f3f70959b39d796ba
Nameless <85962933+Misfiy@users.noreply.github.com>
2023-09-04 13:13:07 +0200

    Kill coroutines better

--- CoroutinesHandler.cs
+++ CoroutinesHandler.cs
@@ -1,8 +1,21 @@
 ﻿namespace AutoBroadcastSystem;
 
+using Exiled.API.Features;
 using MEC;
 
 public static class CoroutinesHandler
 {
-     public static List<CoroutineHandle>? IntervalCoroutines;
+     public static List<CoroutineHandle>? Coroutines;
+     public static void KillCoroutines()
+     {
+          Log.Debug("Killing coroutines");
+          if(Coroutines == null) return;
+          foreach(CoroutineHandle coroutine in Coroutines)
+          {
+               if(coroutine.IsRunning) Timing.KillCoroutines(coroutine);
+               Coroutines.Remove(coroutine);
+               Log.Debug("Killed a coroutine successfully!");
+          }
+          Log.Debug("Killed all coroutines");
+     }
 }
